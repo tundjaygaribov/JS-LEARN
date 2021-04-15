ff88a1a47ac7267985eee83d8e67af26c8406f4e
Tundjay <tolik.garibov974@mail.ru>
2021-04-15 14:59:32 +0300

    main

--- script.js
+++ script.js
@@ -139,4 +139,48 @@
 
 //alert("Hi");
 
+////УСЛОВИЯ/////
+
+//if (4 == 4) {
+//    console.log('Ok!');
+//} else {
+//    console.log('Error!');
+//}
+
+//const num = 50;
+
+//if (num < 49) {
+//    console.log('Error!');
+//} else if (num > 100) {
+//    console.log('Много!');
+//} else {
+//    console.log('Ok!');
+//}
+
+///////ТЕРНАНРНЫЙ аргумент/////
+
+//const num = 50;
+
+//(num === 50) ? console.log('OK!') : console.log('Error');
+//4 + 4 ///бинарный аргумент
+//+'4'; ///унарный аргумент
+
+
+/////////КОНСТРУКЦИЯ SWITCH (строгое сравнение)////////////
+const num = 50;
+
+switch(num) {
+    case 49:
+        console.log('Error!');
+        break;
+    case 100:
+        console.log('Error!');
+        break;
+    case 50:
+        console.log('OK!');
+        break;
+    default:
+        console.log('Не в этот раз!');
+        break;
+}
 
