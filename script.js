bbc34709984c41984850cae76f4f013b748d2c65
Tundjay <tolik.garibov974@mail.ru>
2021-04-15 14:45:37 +0300

    main

--- script.js
+++ script.js
@@ -117,24 +117,26 @@
 //ПОРЯДОК ВЫПОЛНЕНИЯ ОПЕРАТОРОВ
 //const.log( 2+ 2 * 2 === 6)
 
-const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+//const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
 
-const personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false
-};
+//const personalMovieDB = {
+//    count: numberOfFilms,
+//    movies: {},
+//    actors: {},
+//    genres: [],
+//    privat: false
+//};
 
-const a = prompt("Один из последних просмотренных фильмов?", "");
-const b = prompt("На сколько оцените его", "");
-const c = prompt("Один из последних просмотренных фильмов?", "");
-const d = prompt("На сколько оцените его", "");
+//const a = prompt("Один из последних просмотренных фильмов?", "");
+//const b = prompt("На сколько оцените его", "");
+//const c = prompt("Один из последних просмотренных фильмов?", "");
+//const d = prompt("На сколько оцените его", "");
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+//personalMovieDB.movies[a] = b;
+//personalMovieDB.movies[c] = d;
 
-console.log(personalMovieDB);
+//console.log(personalMovieDB);
 
-alert("Hi");
+//alert("Hi");
+
+
