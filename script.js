2857d7118ddc057c8a06163d084677eabb5f6b3f
Tundjay <tolik.garibov974@mail.ru>
2021-04-15 15:54:24 +0300

    main

--- script.js
+++ script.js
@@ -193,21 +193,41 @@
 //}
 
 ////ДЗ//////
-const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+//const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
 
-const personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false
-};
+//const personalMovieDB = {
+//    count: numberOfFilms,
+//    movies: {},
+//    actors: {},
+//    genres: [],
+//    privat: false
+//};
 
-for (let i = 0; i <2 ; i++) {
-    const a = prompt("Один из последних просмотренных фильмов?", ""),
-          b = prompt("На сколько оцените его", "");
+//or (let i = 0; i <2 ; i++) {
+//    const a = prompt("Один из последних просмотренных фильмов?", ""),
+//          b = prompt("На сколько оцените его", "");
+//    
+//    if (a != null && b != null && a != "" && b != "" && a.length <50) {
+//        personalMovieDB.movies[a] = b;
+//        console.log("done");
+//    } else {
+//        console.log("Error");
+//        i--;
+//    }
           
-    personalMovieDB.movies[a] = b;
-}
+//}
 
-console.log(personalMovieDB);
+//console.log(personalMovieDB);
+
+//if (personalMovieDB.count < 10) {
+//    console.log("Просмотрено мало фильмов");
+//} else if (personalMovieDB.count >= 10 && personalMovieDB < 30) {
+//    console.log("Вы классический зритель");
+//} else if (personalMovieDB.count >=30) {
+//    console.log("Вы киноман");
+//} else {
+//    console.log("Error");
+//}
+
+
+
